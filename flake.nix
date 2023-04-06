bce1e20e0aa71d3f8370d8110d50b08f4fc144b5
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-04-07 00:27:18 +0200

    Nice

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.nixpkgs.url = git+file:nixpkgs;
   inputs.home-manager = {
     url = "github:nix-community/home-manager";
     inputs.nixpkgs.follows = "nixpkgs";
