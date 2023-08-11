5ba96cca6411b063689ab7f3cd16fe1060624e3a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-08-11 18:53:08 +0200

    libvirtd

--- configuration.nix
+++ configuration.nix
@@ -483,6 +483,8 @@
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
+  virtualisation.libvirtd.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
