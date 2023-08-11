e8f89b415ef8302471e815627cfe1fe0e9014920
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-08-11 18:48:12 +0200

    Jo

--- configuration.nix
+++ configuration.nix
@@ -481,6 +481,8 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- flake.nix
+++ flake.nix
@@ -7,11 +7,24 @@
   inputs.impermanence.url = "github:nix-community/impermanence/6138eb8e737bffabd4c8fc78ae015d4fd6a7e2fd";
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs = { self, nixpkgs, rust-overlay, ... }@attrs: {
+  outputs = { self, nixpkgs, rust-overlay, ... }@attrs: rec {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [ ./configuration.nix ];
     };
+
+    nixosConfigurations.rpi4 = nixpkgs.lib.nixosSystem {
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel.nix"
+        {
+          nixpkgs.system = "aarch64-linux";
+          boot.supportedFilesystems = nixpkgs.lib.mkForce [ "btrfs" "vfat" ];
+          sdImage.compressImage = false;
+          system.stateVersion = "23.11";
+        }
+      ];
+    };
+    images.rpi4 = nixosConfigurations.rpi4.config.system.build.sdImage;
   };
 }
